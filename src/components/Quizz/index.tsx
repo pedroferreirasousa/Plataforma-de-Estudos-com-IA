cdf0cb2f6f0dd07107ddb8958278457c90c5b553
pedroferreirasousa <pepeferreiras.pf@gmail.com>
2025-04-12 07:05:40 -0300

    resolvendo problema no deploy do vercel

---
+++ src/components/Quizz/index.tsx
@@ -0,0 +1,14 @@
+// "use client";
+// import { useTopic } from '@/contexts/TopicContext';
+
+
+// const Quizz = () => {
+//     return (
+//         <div>
+//             Quizz
+//         </div>
+//     )
+// }
+
+// export default Quizz
+
