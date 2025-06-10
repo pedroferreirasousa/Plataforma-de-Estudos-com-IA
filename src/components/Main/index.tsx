c1eea1e04f76bb1a1b32450501e745ceaa8b1bc5
pedroferreirasousa <pepeferreiras.pf@gmail.com>
2025-06-10 13:38:21 -0300

    feat: ajustes na Main

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -2,9 +2,15 @@
 import StudyPlanner from '../StudyPlanner/StudyPlanner';
 import Quiz from '../Quizz';
 import styles from './Main.module.scss';
+import Image from 'next/image';
+import Logo from '../../../public/logo.svg'
 
 function Main() {
     return (
+        <div className={styles.section}>
+        <div className={styles.logo}>
+            <Image src={Logo} alt="Logo" width={100} height={100} />
+        </div>
         <div className={styles.container}>
             <div id='planner' className={styles.left}>
                 <StudyPlanner />
@@ -12,6 +18,7 @@ function Main() {
             <div id='quiz' className={styles.right}>
                 <Quiz />
             </div>
+        </div>
         </div>
     );
 }
